c57cb068880eb41c1dbfe0db09f50cff50e77560
neil.mispelaar <neil.mispelaar@outlook.com>
2019-04-03 13:58:00 -0400

    Add some things and the editor config file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+    # Config default locale handing 
+    # https://dev.to/morinoko/setting-up-i18n-for-rails-with-locales-from-url-params-38pg
+    # We want the following paths:
+    #       http://domain.com/en/stuff
+    #       http://domain.com/fr/stuff
+    #
+
+    before_action :set_locale
+
+    def set_locale
+        I18n.locale = params[:locale] || I18n.default_locale
+    end
+
+    def default_url_options
+        { locale: I18n.locale }
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   
-    root 'static_pages#home'
+
+
+    scope "(:locale)", locale: /en|de/ do
+        root 'static_pages#home'
+    end
+
+    
+    
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    # Use the following resource to sort out routes
+    # https://dhampik.com/blog/rails-routes-tricks-with-locales
+    # 
+
+
 
 end
