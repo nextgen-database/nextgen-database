3e961b6f65f611534f257af1d521b32ddc63be07
neil.mispelaar <neil.mispelaar@outlook.com>
2019-06-02 12:22:01 -0400

    Add SDGs to the new profile and view profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -42,6 +42,7 @@ class ProfilesController < ApplicationController
 				demographic_ids:[],
 				development_actor_ids:[],
 				research_method_ids:[],
+				sustainable_development_goal_ids:[],
 				affiliations_attributes: [:id, :organisation_id, :_destroy,
 					positions_attributes: [:id, :title_id, :department_en, :department_fr, :faculty_en, :faculty_fr, :_destroy],
 					affiliation_positions: [:id, :organisation_id, :position_id, :_destroy]
