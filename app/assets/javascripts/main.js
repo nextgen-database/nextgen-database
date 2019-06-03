02e2367595b6ec7b0d7719a6ef3f9e45f6e4ca2d
neil.mispelaar <neil.mispelaar@outlook.com>
2019-06-02 21:02:35 -0400

    Add header links and styles

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,16 @@
+/*!
+ * Main Application JS File
+ *
+ * Author: Neil Mispelaar
+ *
+ */
+
+// Run this when JQuery is ready
+jQuery(document).ready(function ($) {
+
+	// Activate the mobile menu
+	mobileMenu.init({
+		menuContainerClassName: "ng-navbar-links-container"
+	});
+
+});
