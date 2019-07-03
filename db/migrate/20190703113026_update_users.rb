64f0a8bf7ec0306bfa7644e7e7f2a0e0c56aff73
neil.mispelaar <neil.mispelaar@outlook.com>
2019-07-03 09:33:00 -0400

    Update User Table

--- db/migrate/20190703113026_update_users.rb
+++ db/migrate/20190703113026_update_users.rb
@@ -30,7 +30,7 @@ class UpdateUsers < ActiveRecord::Migration[5.2]
 
 		## Lockable
 		add_column 		:users, :failed_attempts, :integer, default: 0, null: false # Only if lock strategy is :failed_attempts
-		add_column		:users, :unlock_token # Only if unlock strategy is :email or :both
+		add_column		:users, :unlock_token, :string # Only if unlock strategy is :email or :both
 		add_column		:users, :locked_at, :datetime
 
 		# t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -38,9 +38,9 @@ class UpdateUsers < ActiveRecord::Migration[5.2]
 		# t.datetime :locked_at
 
 
-	end
+		add_index :users, :confirmation_token,   unique: true
+	    add_index :users, :unlock_token,         unique: true
 
-	add_index :users, :confirmation_token,   unique: true
-    add_index :users, :unlock_token,         unique: true
+	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_040132) do
+ActiveRecord::Schema.define(version: 2019_07_03_113026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -196,9 +196,23 @@ ActiveRecord::Schema.define(version: 2019_06_25_040132) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false
+    t.integer "role"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   add_foreign_key "affiliations", "organisations"
