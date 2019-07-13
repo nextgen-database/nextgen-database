2d5ffe92348327da2171872f76a7fc4b2b09b574
neil.mispelaar <neil.mispelaar@outlook.com>
2019-07-13 11:33:07 -0400

    Add comment for future todo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,8 @@ class ApplicationController < ActionController::Base
         { locale: I18n.locale }
 	end
 
+    # After someone signs in send them to the account page
+    # TODO: If the user is an admin send them to the admin panel
 	def after_sign_in_path_for(resource)
 		stored_location_for(resource) || account_path
 	end
