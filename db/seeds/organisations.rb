61635604e1f0037897a34f3b1c18fa0442503912
neil.mispelaar <neil.mispelaar@outlook.com>
2019-07-16 15:17:45 -0400

    Seed organisations

--- db/seeds/organisations.rb
+++ db/seeds/organisations.rb
@@ -21,7 +21,355 @@ organisations_list = [
 	{
 		english: "McGill University",
 		french: "McGill University"
-	}
+	},
+  	{
+		english:"Menno Simons College",
+		french:"Menno Simons College"
+	},
+	{
+		english:"Niagara College",
+		french:"Niagara College"
+	},
+	{
+		english:"Queens University",
+		french:"Queens University"
+	},
+	{
+		english:"St. Francis Xavier",
+		french:"St. Francis Xavier"
+	},
+	{
+		english:"Université du Québec à Montréal",
+		french:"Université du Québec à Montréal"
+	},
+	{
+		english:"Trent University",
+		french:"Trent University"
+	},
+	{
+		english:"Université de Montréal",
+		french:"Université de Montréal"
+	},
+	{
+		english:"Université de Laval",
+		french:"Université de Laval"
+	},
+	{
+		english:"University of Calgary",
+		french:"University of Calgary"
+	},
+	{
+		english:"University of Guelph",
+		french:"University of Guelph"
+	},
+	{
+		english:"University of Northern British Columbia",
+		french:"University of Northern British Columbia"
+	},
+	{
+		english:"University of Ottawa",
+		french:"University of Ottawa"
+	},
+	{
+		english:"University of Toronto Scarborough",
+		french:"University of Toronto Scarborough"
+	},
+	{
+		english:"University of Waterloo",
+		french:"University of Waterloo"
+	},
+	{
+		english:"University of Winnipeg",
+		french:"University of Winnipeg"
+	},
+	{
+		english:"Vancouver Island University",
+		french:"Vancouver Island University"
+	},
+	{
+		english:"Wilfred Laurier University",
+		french:"Wilfred Laurier University"
+	},
+	{
+		english:"York University",
+		french:"York University"
+	},
+	{
+		english:"University of Quebec in Outaouais",
+		french:"University of Quebec in Outaouais"
+	},
+	{
+		english:"RESULTS Canada",
+		french:"RESULTS Canada"
+	},
+	{
+		english:"Right To Play International",
+		french:"Right To Play International"
+	},
+	{
+		english:"University of Toronto",
+		french:"University of Toronto"
+	},
+	{
+		english:"World Vision Canada",
+		french:"World Vision Canada"
+	},
+	{
+		english:"University of Waterloo ",
+		french:"University of Waterloo "
+	},
+	{
+		english:"Coady International Institute",
+		french:"Coady International Institute"
+	},
+	{
+		english:"The Balsillie School of International Affairs",
+		french:"The Balsillie School of International Affairs"
+	},
+	{
+		english:"Carleton University",
+		french:"Carleton University"
+	},
+	{
+		english:"University of Regina",
+		french:"University of Regina"
+	},
+	{
+		english:"Saint Mary's University",
+		french:"Saint Mary's University"
+	},
+	{
+		english:"University of Manitoba",
+		french:"University of Manitoba"
+	},
+	{
+		english:"Manitoba Council for International Cooperation",
+		french:"Manitoba Council for International Cooperation"
+	},
+	{
+		english:"Canadian Foodgrains Bank",
+		french:"Canadian Foodgrains Bank"
+	},
+	{
+		english:"CARE Canada",
+		french:"CARE Canada"
+	},
+	{
+		english:"Aga Khan Foundation of Canada",
+		french:"Aga Khan Foundation of Canada"
+	},
+  	{
+		english:"Resource Efficient Agricultural Production Canada (REAP)",
+		french:"Resource Efficient Agricultural Production Canada (REAP)"
+	},
+	{
+		english:"Plan International Canada",
+		french:"Plan International Canada"
+	},
+	{
+		english:"Inter Pares",
+		french:"Inter Pares"
+	},
+	{
+		english:"Besa",
+		french:"Besa"
+	},
+	{
+		english:"Cape Breton University",
+		french:"Cape Breton University"
+	},
+	{
+		english:"University of British Columbia ",
+		french:"University of British Columbia "
+	},
+	{
+		english:"Federation of Canadian Municipalities",
+		french:"Federation of Canadian Municipalities"
+	},
+	{
+		english:"Global Centre for Pluralism ",
+		french:"Global Centre for Pluralism "
+	},
+	{
+		english:"USC Canada",
+		french:"USC Canada"
+	},
+	{
+		english:"Women, Peace and Security Network - Canada",
+		french:"Women, Peace and Security Network - Canada"
+	},
+	{
+		english:"Humber College",
+		french:"Humber College"
+	},
+	{
+		english:"Farm Radio International",
+		french:"Farm Radio International"
+	},
+	{
+		english:"AidWatch Canada",
+		french:"AidWatch Canada"
+	},
+	{
+		english:"Fondation Mères du Monde en Santé",
+		french:"Fondation Mères du Monde en Santé"
+	},
+	{
+		english:"Chaire de Recherche en Développement des Collectivités (CRDC)",
+		french:"Chaire de Recherche en Développement des Collectivités (CRDC)"
+	},
+	{
+		english:"L'Association Québécoise des Organismes de Coopération Internationale (AQOCI)",
+		french:"L'Association Québécoise des Organismes de Coopération Internationale (AQOCI)"
+	},
+	{
+		english:"CODE ",
+		french:"CODE "
+	},
+	{
+		english:"Victoria International Development Education Association (VIDEA)",
+		french:"Victoria International Development Education Association (VIDEA)"
+	},
+	{
+		english:"CUSO International",
+		french:"CUSO International"
+	},
+	{
+		english:"University of Western Ontario",
+		french:"University of Western Ontario"
+	},
+	{
+		english:"University of Alberta",
+		french:"University of Alberta"
+	},
+	{
+		english:"Memorial University of Newfoundland",
+		french:"Memorial University of Newfoundland"
+	},
+	{
+		english:"University of Saskatchewan",
+		french:"University of Saskatchewan"
+	},
+	{
+		english:"Canadian Council for International Cooperation",
+		french:"Canadian Council for International Cooperation"
+	},
+	{
+		english:"Capilano University ",
+		french:"Capilano University "
+	},
+	{
+		english:"The King's University ",
+		french:"The King's University "
+	},
+	{
+		english:"Action Canada for Sexual Health and Rights",
+		french:"Action Canada for Sexual Health and Rights"
+	},
+	{
+		english:"MacEwan University",
+		french:"MacEwan University"
+	},
+	{
+		english:"Islamic Relief Canada ",
+		french:"Islamic Relief Canada "
+	},
+	{
+		english:"Mt. Allison University",
+		french:"Mt. Allison University"
+	},
+	{
+		english:"Teluq",
+		french:"Teluq"
+	},
+	{
+		english:"Sherbrooke University",
+		french:"Sherbrooke University"
+	},
+	{
+		english:"St. Mary's University",
+		french:"St. Mary's University"
+	},
+	{
+		english:"University of New Brunswick",
+		french:"University of New Brunswick"
+	},
+	{
+		english:"International Development Research Centre",
+		french:"International Development Research Centre"
+	},
+	{
+		english:"World University Service of Canada",
+		french:"World University Service of Canada"
+	},
+	{
+		english:"Université du Québec à Chicoutimi ",
+		french:"Université du Québec à Chicoutimi "
+	},
+	{
+		english:"School of Oriental and African Studies",
+		french:"School of Oriental and African Studies"
+	},
+	{
+		english:"Laurentian University",
+		french:"Laurentian University"
+	},
+	{
+		english:"HEC Montreal",
+		french:"HEC Montreal"
+	},
+	{
+		english:"University of Massachusetts Boston",
+		french:"University of Massachusetts Boston"
+	},
+	{
+		english:"Social Sciences and Humanities Research Council",
+		french:"Social Sciences and Humanities Research Council"
+	},
+	{
+		english:"Simon Fraser University",
+		french:"Simon Fraser University"
+	},
+	{
+		english:"Balsillie School of International Affairs",
+		french:"Balsillie School of International Affairs"
+	},
+	{
+		english:"Canadian Association for the Study of International Development (CASID)",
+		french:"Canadian Association for the Study of International Development (CASID)"
+	},
+	{
+		english:"Ideal World ",
+		french:"Ideal World "
+  	},
+  	{
+		english:"Diamond Development Initiative",
+		french:"Diamond Development Initiative"
+  	},
+  	{
+		english:"OXFAM Canada",
+		french:"OXFAM Canada"
+  	},
+  	{
+		english:"Veterinarians without Borders ",
+		french:"Veterinarians without Borders "
+  	},
+  	{
+		english:"Task Team on CSO Development Effectiveness and Enabling Environment (Secretariat)",
+		french:"Task Team on CSO Development Effectiveness and Enabling Environment (Secretariat)"
+  	},
+  	{
+    	english:"British Columbia Council for International Cooperation",
+    	french:"British Columbia Council for International Cooperation"
+  	},
+	{
+		english:"Brookings Institution",
+		french:"Brookings Institution"
+  	},
+  	{
+		english:"afield",
+		french:"afield"
+  	}
 ]
 
 # Function to only add unique values
