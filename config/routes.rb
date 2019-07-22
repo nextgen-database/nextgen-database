92896df689d500bdd53d7951f02d396faa44b089
neil.mispelaar <neil.mispelaar@outlook.com>
2019-07-22 16:44:18 -0400

    Remove extra comments from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-#	devise_for :users
-
 	# Devise authentication routes
 	devise_for :users
 
@@ -49,31 +47,5 @@ Rails.application.routes.draw do
 	root to: redirect("/#{I18n.default_locale}", status: 302), as: :redirected_root
 
 	get "/*path", to: redirect("/#{I18n.default_locale}/%{path}", status: 302), constraints: {path: /(?!(#{I18n.available_locales.join("|")})\/).*/}, format: false
-
-
-
-
-	# https://everydayrails.com/2012/07/31/rails-admin-panel-from-scratch.html
-	# https://roberteshleman.com/blog/2014/08/14/using-rails-namespaces-for-admin-actions/
-	#
-
-
-	#devise_for :admins, path: 'admin', skip: :registrations
-
-
-	#scope "/:locale", locale: /#{I18n.available_locales.join("|")}/ do
-    #	root to: redirect("/%{locale}/static_pages#home", status: 302)
-  	#end
-
-#    scope "(:locale)", locale: /en|de/ do
-#        root 'static_pages#home'
-#    end
-
-    #get  'static_pages/about'
-
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
-
 
 end
