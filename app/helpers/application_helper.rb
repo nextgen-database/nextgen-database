7da69c813380443a98131cd5b61449f47170a4c9
neil.mispelaar <neil.mispelaar@outlook.com>
2019-07-25 13:54:52 -0400

    Add title logic and translations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+	def title
+
+		divider_site_title = " | "
+		base_site_title = t("site_name")
+
+		if content_for?(:title)
+		  # allows the title to be set in the view by using t(".title")
+		  content_for :title
+
+		else
+		  # look up translation key based on controller path, action name and .title
+		  # this works identical to the built-in lazy lookup
+		  t("#{ controller_path.tr('/', '.') }.#{ action_name }.title", default: "") + divider_site_title + base_site_title
+		end
+	end
+
 end
